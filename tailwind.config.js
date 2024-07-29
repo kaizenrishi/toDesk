46fd200d17dbcef0efff74fe7af85cfb32ef3192
Rishabh Rawat <137486565+kaizenrishi@users.noreply.github.com>
2024-07-29 16:55:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    
+    extend: {
+      colors:{
+        primary: '#4B50F3',
+        btb: '#FFF6CC',
+        int:'#713F12'
+      },
+    },
+  },
+  plugins: [],
+}
+
